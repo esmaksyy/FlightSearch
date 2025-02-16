bc4e0a3c5a22ba1340af21bd0ca166c62f52916a
Esma <esmaksyy@gmail.com>
2025-02-16 23:10:55 +0300

    Add input fields for flight search parameters

--- src/components/FlightSearch.jsx
+++ src/components/FlightSearch.jsx
@@ -7,6 +7,23 @@ const FlightSearch = () => {
   return (
     <div>
       <h2>Flight Search</h2>
+      <input
+        type="text"
+        placeholder="From"
+        value={searchParams.from}
+        onChange={(e) => setSearchParams({ ...searchParams, from: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="To"
+        value={searchParams.to}
+        onChange={(e) => setSearchParams({ ...searchParams, to: e.target.value })}
+      />
+      <input
+        type="date"
+        value={searchParams.date}
+        onChange={(e) => setSearchParams({ ...searchParams, date: e.target.value })}
+      />
     </div>
   );
 };
